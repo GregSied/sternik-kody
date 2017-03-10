bc9b3f943acb30f17df25930fb7a7803f033199b
Arkadiusz Cacko <cackoarek@gmail.com>
2017-03-10 12:44:57 +0100

    Zadanie 7

---
+++ src/pl/sages/zadanka/Zadanie7.java
@@ -0,0 +1,19 @@
+package pl.sages.zadanka;
+
+public class Zadanie7 {
+	
+	public static void main(String[] args) {
+		int[] table = {1, 2, 3, 4, 5, 6, 7};
+		podzielne(table, 3);
+	}
+	
+	public static void podzielne(int[] table, int number){
+		for (int i = 0; i < table.length; i++) {
+			if(table[i] % number == 0){
+				System.out.println("Znalazlem liczbe: " + table[i] + ", na pozycji: " + i);
+				break;
+			}
+		}
+		
+	}
+}
